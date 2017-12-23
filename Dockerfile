8ca0fef06bfb2c6c981825619740f8c8af53ec42
Omar Mohsen <o.mohsen@arabhosters.com>
2017-12-23 14:10:13 +0200

    Modifying entrypint script

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM hhcordero/docker-jmeter-server
 
-MAINTAINER Hector Cordero <hhcordero@gmail.com>
 
-ENV TEST_DIR default
-ENV TEST_PLAN test-plan
+ENV TEST_DIR /tmp
+ENV TEST_PLAN test-plan.jmx
 ENV REMOTE_HOSTS 127.0.0.1
+RUN chmod g+rwx -R /usr/local/apache-jmeter-2.13/
 
-COPY load_tests /
+COPY load_tests $TEST_DIR 
 COPY docker-entrypoint.sh /
 
 ENTRYPOINT ["/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
-set -e
+#set -e
 freeMem=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
 s=$(($freeMem/10*8))
 x=$(($freeMem/10*8))
 n=$(($freeMem/10*2))
 export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
-$JMETER_BIN/jmeter \
+
+
+execute_jmeter(){
+jmeter_command=`$JMETER_BIN/jmeter \
     -n \
     -D "java.rmi.server.hostname=${IP}" \
     -D "client.rmi.localport=${RMI_PORT}" \
-    -t "/load_tests/${TEST_DIR}/${TEST_PLAN}.jmx" \
-    -l "/load_tests/${TEST_DIR}/${TEST_PLAN}.jtl" \
-    -R $REMOTE_HOSTS
+    -t "${TEST_DIR}/${TEST_PLAN}" \
+    -l "${TEST_DIR}/${TEST_PLAN}.jtl" \
+    -R $REMOTE_HOSTS`
+echo $jmeter_command|grep -E -v 'Engine is busy|java.net.UnknownHostException|java.net.SocketException' 
+if [ $? -eq 1 ];then
+  echo $jmeter_command
+  exit 1
+fi
 exec tail -f jmeter.log
+}
+execute_jmeter
+
+
